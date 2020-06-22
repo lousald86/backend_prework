a8f41e856a6507777a4f4cb8e8128e8cff2ac632
—replace-all <lousald86@gmail.comssh-keygen -t rsa -C lousald86@gmail.com>
2020-06-22 14:34:53 -0400

    Adds Day 5 Work

--- day_5/ex39.rb
+++ day_5/ex39.rb
@@ -1,73 +1,25 @@
-# create a mapping of state to abbreviation
 states = {
-  'Oregon' => 'OR',
-  'Florida' => 'FL',
-  'California' => 'CA',
-  'New York' => 'NY',
-  'Michigan' => 'MI'
+  'Oregon' => 'OR'
+  'Florida' => 'FL'
+  'California' => 'CA'
+  'New York' => 'NY'
+  'Michigan' => "MI'
 }
 
-# create a basin set of states and some cities in them
-
 cities = {
-  'CA' => 'San Francisco',
-  'MI' => 'Detroit',
+  'CA' => 'San Francisco'
+  'MI' => 'Detroit'
   'FL' => 'Jacksonville'
 }
 
-# add some more cities
-cities['NY'] = "New York"
-cities['OR'] = "Portland"
+#adds more cities
+cities['NY'] = 'New York'
+cities['OR'] = 'Portland'
 
-# puts out some cities
-puts '-' * 10
-puts "NY State has: #{cities['NY']}"
-puts "OR State has: #{cities['OR']}"
+#prints out cities
+puts "NY state has: #{cities['NY']}"
 
-# puts some states
-puts '-' * 10
-puts "Michigan's abbreviation is: #{states['Michigan']}"
-puts "Florida's abbreviation is: #{states['Florida']}"
-
-# do it bu using the state then cities dict
-puts '-' * 10
-puts "Michigan has: #{cities[states['Michigan']]}"
-puts "Florida has: #{cities[states['Florida']]}"
-
-# puts eer state abbreviation
-puts '-' * 10
-states.each do |state, abbrev|
-  puts "#{state} is abbreviated #{abbrev}"
-end
-
-#puts every city in state
-puts '-' * 10
-cities.each do |abbrev, city|
-  puts "#{abbrev} has the city #{city}"
-end
-
-# now both at the same time
-puts '-' * 10
-states.each do |state, abbrev|
-  city = cities[abbrev]
-  puts "#{state} is abbreviated #{abbrev} and has city #{city}"
-end
-
-puts '-' * 10
-# by default ruby says "nil" when something isn't there
-state = states['Texas']
-
-if !state
-  puts "Sorry, no Texas."
-end
-
-#default values using ||= with the nil result
-city = cities['TX']
-city ||= 'Does Not Exist'
-puts "The city for the state 'TX' is: #{city}"
-puts '-' * 15
-
-# STUDY DRILLS
+# STUDY DRILLS(cool stuff by brad to keep as notes)
 
 # my own mappin of cities and states
 mtn_states = {'Colorado' => 'CO', 'New Mexico' => 'NM', 'Wyoming' => 'WY'}
--- day_5/exercises/hashes.rb
+++ day_5/exercises/hashes.rb
@@ -3,12 +3,12 @@
 # file from your terminal with the command `ruby day_6/exercises/hashes.rb`
 
 # example: Write code below to print a hash that holds grocery store inventory
-foods = {apples: 23, grapes: 507, eggs: 48}
-p foods
+food = {ribs: 3, fries: 20, corn: 10}
+p food
 
 # Write code below that will print a hash of animals and their number
 # at the zoo. (an inventory of animals)
-zoo = {koalas: 4, giraffes: 7, otters: 8}
+zoo = {gazelles: 15, gorillas: 8, toucans: 4}
 p zoo
 
 # Using the zoo that you created above, print all the keys in the hash.
@@ -19,8 +19,8 @@ p zoo.values
 
 # Using the zoo that you created above, print the value of the first item in
 # the hash
-p zoo[:koalas]
+p zoo[:gazelles]
 
 # Add an animal to the zoo hash and print the updated hash.
-zoo[:stingray] = 10
+zoo[:rhino] = 10
 p zoo
